c526bb791360f6f409ad9651ae19544f487aa80f
Benedicta Edita <benedictaedita@Benedictas-MacBook-Pro.local>
2024-09-02 15:42:36 +0700

    initial commit

---
+++ Assignment1/FirstTask/add_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Enter the first integer: ";
+    cin >> a;
+    cout << "Enter the second integer: ";
+    cin >> b;
+    cout << a << " + " << b << " = " << a+b << endl;
+}
